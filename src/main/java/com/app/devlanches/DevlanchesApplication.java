1a6a992e9124618fd07ebb47ad4f1a7bdf0f46af
Clesyo <clesyo.silva@gmail.com>
2021-08-27 16:00:35 -0300

    Commit inicial

---
+++ src/main/java/com/app/devlanches/DevlanchesApplication.java
@@ -0,0 +1,13 @@
+package com.app.devlanches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlanchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlanchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/devlanches/DevlanchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.devlanches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlanchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
