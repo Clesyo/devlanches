381bd4a1dc9aae925ae0380b604ba183b0364ace
Clesyo <clesyo.silva@gmail.com>
2021-08-29 07:10:41 -0300

    Criando home para api

---
+++ src/main/java/com/app/devlanches/api/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.app.devlanches.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "<h1>Bem vindo ao Api - Dev Lanches</h1>";
+	}
+}
